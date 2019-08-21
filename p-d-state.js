1ff4b47f4ad25268233980afbe59648e6a8747f2
bahrus <anderson.bruce.b@gmail.com>
2019-08-21 15:34:10 -0400

    ...

--- p-d-state.js
+++ p-d-state.js
@@ -1,6 +1,25 @@
 import { PDX } from './p-d-x.js';
 import { define } from 'trans-render/define.js';
+const guid = 'guid';
 export class PDState extends PDX {
     static get is() { return 'p-d-state'; }
+    get guid() {
+        return this._guid;
+    }
+    set guid(val) {
+        this.attr(guid, val);
+    }
+    static get observedAttributes() {
+        return super.observedAttributes.concat([guid]);
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        this.propUp(['guid']);
+        const xtalWatch = document.createElement('xtal-state-watch');
+        xtalWatch.addEventListener('history-changed', e => {
+            this.pass(e);
+        });
+        this.appendChild(xtalWatch);
+    }
 }
 define(PDState);
--- p-d-state.ts
+++ p-d-state.ts
@@ -1,8 +1,31 @@
 import {PDX} from './p-d-x.js';
 import {define} from 'trans-render/define.js';
+import {XtalStateWatch} from 'xtal-state/xtal-state-watch.js';
+
+const guid = 'guid';
 
 export class PDState extends PDX{
     static get is(){return 'p-d-state';}
+    //TODO:  shared mixin
+    private _guid!: string;
+    get guid(){
+        return this._guid;
+    }
+    set guid(val){
+        this.attr(guid, val);
+    }
+    static get observedAttributes() {
+        return super.observedAttributes.concat([guid]);
+    }
+    connectedCallback(){
+        super.connectedCallback();
+        this.propUp(['guid']);
+        const xtalWatch = document.createElement('xtal-state-watch') as XtalStateWatch;
+        xtalWatch.addEventListener('history-changed', e =>{
+            this.pass(e);
+        });
+        this.appendChild(xtalWatch);
+    }
 }
 
 define(PDState);
