76ce32a35c8188d51e8deb90ddf9749da5b040b5
bahrus <anderson.bruce.b@gmail.com>
2019-08-25 11:16:23 -0400

    ...

--- demo/tsSyntax/attempt1.ts
+++ demo/tsSyntax/attempt1.ts
@@ -1,38 +1,56 @@
 const myCustomEl1 = {
-    props:{
-        MyFirstProp:{
-            type: String,
-            id: Symbol()
-        }
-    },
-    events:{
-        'item-selected': {
-            name: 'item-selected',
-            id: Symbol(),
-            detail:{
-                value:{
-                    id: Symbol()
-                }
-            }
-        }
+  props: {
+    MyFirstProp: {
+      type: String,
+      id: Symbol()
     }
-}
+  },
+  events: {
+    "item-selected": {
+      id: Symbol(),
+      detail: {
+        value: {
+          id: Symbol()
+        }
+      }
+    }
+  }
+};
 
 const myCustomEl2 = {
-    props:{
-        MySecondProp: {
-            type: String,
-            id: Symbol('')
-        }
+  props: {
+    MySecondProp: {
+      type: String,
+      id: Symbol("")
     }
-}
+  }
+};
+const myCustomEl1Instance = Symbol();
+const myCustomEl2Instance = Symbol();
+const fragment = [
+  {
+    tag: myCustomEl1,
+    id: myCustomEl1Instance,
+    propsVals: [
+      {
+        id: myCustomEl1.props.MyFirstProp.id,
+        value: "hello"
+      }
+    ]
+  },
+  {
+    tag: myCustomEl2,
+    id: myCustomEl2Instance
+  }
+];
 
-const fragment = [myCustomEl1, myCustomEl2];
-const link = {
-    source: myCustomEl1,
+const links = [
+  {
+    source: myCustomEl1Instance,
     event: myCustomEl1.events["item-selected"].id,
     sourceProp: myCustomEl1.props.MyFirstProp.id,
     sourceVal: myCustomEl1.events["item-selected"].detail.value.id,
-    dest: myCustomEl2,
+    dest: myCustomEl2Instance,
     destProp: myCustomEl2.props.MySecondProp.id
-}
+  }
+];
