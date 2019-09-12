78fb22450eb241b71bb0e07675150bd8a196fa7a
bahrus <anderson.bruce.b@gmail.com>
2019-09-12 12:11:43 -0400

    ...

---
+++ demo/xtal-json-merge-pd.ts
@@ -0,0 +1,8 @@
+import {Pixin} from '../p-ixin.js';
+import {XtalJSONMerge} from '../node_modules/xtal-json-merge/xtal-json-merge.js';
+import {define} from 'trans-render/define.js';
+
+export class XtalJsonMergePD extends Pixin(XtalJSONMerge){
+    static get is(){return 'xtal-json-merge-pd';}
+}
+define(XtalJsonMergePD);
