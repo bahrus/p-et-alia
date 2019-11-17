0b17085ed7c86a766a6b21ea8cc90cd3dad30ce4
Bruce Anderson <anderson.bruce.b@gmail.com>
2019-11-16 21:11:52 -0500

    ...

--- test/p-d-tests.ts
+++ test/p-d-tests.ts
@@ -12,7 +12,7 @@ async function customTests(page: Page) {
     } as Test;
     TapeTestRunner.test('testing dev.html', (t: any) => {
         t.equal(errorTags.length, 0);
-        t.equals(endings.length, 2);
+        t.equals(endings.length, 3);
         t.end();
     });
 
