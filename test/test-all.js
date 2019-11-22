93a8fef7ab74cd1b1b1e25daa360a11a66286d81
bahrus <anderson.bruce.b@gmail.com>
2019-11-22 14:03:23 -0500

    ...

---
+++ test/test-all.js
@@ -0,0 +1,15 @@
+const xt = require('xtal-test/index');
+(async () => {
+    const passed = await xt.runTests([
+        {
+            path: 'test/fly-d.html',
+            expectedNoOfSuccessMarkers: 1,
+        },
+    ]);
+    if (passed) {
+        console.log("Tests Passed.  Have a nice day.");
+    }
+    else {
+        throw "Tests Failed.  Try, try again.";
+    }
+})();
---
+++ test/test-all.ts
@@ -0,0 +1,19 @@
+import { IXtalTestRunner, IXtalTestRunnerOptions } from 'xtal-test/index.js';
+const xt = require('xtal-test/index') as IXtalTestRunner;
+
+
+(async () => {
+    const passed = await xt.runTests([
+        {
+            path: 'test/fly-d.html',
+            expectedNoOfSuccessMarkers: 1,
+
+        },
+    ]);
+    if(passed){
+        console.log("Tests Passed.  Have a nice day.");
+    }else{
+        throw "Tests Failed.  Try, try again."
+    }
+})();
+
