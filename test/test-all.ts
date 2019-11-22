33165c3eec597a326c346698ea76358e5f649d83
bahrus <anderson.bruce.b@gmail.com>
2019-11-22 14:15:34 -0500

    ...

--- test/test-all.ts
+++ test/test-all.ts
@@ -6,7 +6,7 @@ const xt = require('xtal-test/index') as IXtalTestRunner;
     const passed = await xt.runTests([
         {
             path: 'test/fly-d.html',
-            expectedNoOfSuccessMarkers: 1,
+            expectedNoOfSuccessMarkers: 4,
 
         },
     ]);
