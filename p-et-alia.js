89cc13bb799b29d7e159f43720ceafdacc250ec2
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-06-12 23:27:28 -0400

    ...

--- p-et-alia.js
+++ p-et-alia.js
@@ -1,6 +1,7 @@
-import './p-d.js';
-import './p-d-r.js';
-import './p-d-x.js';
-import './p-u.js';
-import './p-h-d.js';
-import './p-w.js';
+"use strict";
+import('./p-d.js');
+import('./p-d-r.js');
+import('./p-d-x.js');
+import('./p-u.js');
+import('./p-h-d.js');
+import('./p-w.js');
--- p-et-alia.ts
+++ p-et-alia.ts
@@ -1,6 +1,6 @@
-import './p-d.js';
-import './p-d-r.js';
-import './p-d-x.js';
-import './p-u.js';
-import './p-h-d.js';
-import './p-w.js';
+import('./p-d.js');
+import('./p-d-r.js');
+import('./p-d-x.js');
+import('./p-u.js');
+import('./p-h-d.js');
+import('./p-w.js');
